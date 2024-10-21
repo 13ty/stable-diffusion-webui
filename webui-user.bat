2922a6c4c2786d8f861e402a49088b84d5ce1779
13ty <166788677+13ty@users.noreply.github.com>
2024-10-21 23:59:46 +0200

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -2,7 +2,7 @@
 
 set PYTHON=
 set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+set VENV_DIR=C:\Users\Hargrat\Documents\GitHub\stable-diffusion-webui\venv
+set COMMANDLINE_ARGS=--api --api-auth hargrat:1111 --share --lowvram
 
 call webui.bat
